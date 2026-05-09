0b44024bca9cdf03df15a3a8e3692804edf7b037
Zcaroll <zcarroll.bus@gmail.com>
2026-05-09 23:51:45 +0000

    added discount lists, and added functionality to the discount function

--- cart_discount/price_discount.py
+++ cart_discount/price_discount.py
@@ -2,6 +2,9 @@ def main():
 
     print(discount([10, 4, 20]))  # Expect this to print 4
     # what other lists might this function be called with?
+    print(discount([5, 9]))
+    print(discount([7, 2, 9, 14]))
+    print(discount([10, 10, 10]))
     
 
 def discount(item_prices):
@@ -10,7 +13,10 @@ def discount(item_prices):
     Example: if this function is called with a list of [10, 4, 20] then return 4.
     """
 
-    pass  # todo replace this line with your code 
+    if len(item_prices) < 3:
+        return 0
+
+    return min(item_prices)
 
 
 if __name__ == '__main__':
